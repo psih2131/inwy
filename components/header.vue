6eb3edc1b2f970964682f373c810a4d3e957afbc
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-09 14:08:23 +0300

    add header

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,45 @@
 <template>
     <header class="header">
+        <div class="header-container">
+            <nav class="header__nav">
+                <ul class="header__list">
+                    <li class="header__list-element">
+                        <a href="" class="header__list-element-link">
+                            <div class="header__list-element-link-col">
+                                <span class="header__list-element-link-text">услуги</span>
+                                <span class="header__list-element-link-text">услуги</span>
+                            </div>
+                        </a>
+                    </li>
 
+                    <li class="header__list-element">
+                        <a href="" class="header__list-element-link">
+                            <div class="header__list-element-link-col">
+                                <span class="header__list-element-link-text">о нас</span>
+                                <span class="header__list-element-link-text">о нас</span>
+                            </div>
+                        </a>
+                    </li>
+
+                    <li class="header__list-element">
+                        <a href="" class="header__list-element-link">
+                            <div class="header__list-element-link-col">
+                                <span class="header__list-element-link-text">блог</span>
+                                <span class="header__list-element-link-text">блог</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <a href="" class="header__logo">
+                <img src="@/assets/images/logo.png" alt="" class="header__logo-img">
+            </a>
+
+            <div class="header__right">
+                <button class="header__btn btn-v1">обсудить проект</button>
+            </div>
+        </div>
     </header>
     
 </template>
