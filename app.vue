a520691b0ef2b21292cd76b852850ae13cf056da
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-07-16 01:50:36 +0300

    fix

--- app.vue
+++ app.vue
@@ -15,19 +15,19 @@
 // import { useCounterStore } from '@/stores/counter'
 import { ref, onMounted, onBeforeUnmount, computed, watch  } from 'vue';
 
-// onMounted(() => {
-//   const cursor = document.getElementById('custom-cursor')
+onMounted(() => {
+  const cursor = document.getElementById('custom-cursor')
 
-//   const move = (e) => {
-//     cursor.style.top = `${e.clientY}px`
-//     cursor.style.left = `${e.clientX}px`
-//   }
+  const move = (e) => {
+    cursor.style.top = `${e.clientY}px`
+    cursor.style.left = `${e.clientX}px`
+  }
 
-//   window.addEventListener('mousemove', move)
+  window.addEventListener('mousemove', move)
 
-//   onBeforeUnmount(() => {
-//     window.removeEventListener('mousemove', move)
-//   })
-// })
+  onBeforeUnmount(() => {
+    window.removeEventListener('mousemove', move)
+  })
+})
 
 </script>
